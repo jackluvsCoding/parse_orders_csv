57ed678cf23f9000f1d9a63de55adbbd38bc77ac
Jack Hysell <jackhysell@gmail.com>
2026-06-07 18:12:32 -0500

    Remove unused legacy Product class

--- objects/product.py
+++
@@ -1,6 +0,0 @@
-class Product:
-    def __init__(self, product_id, product_name, product_options, quantity):
-        self.product_id = product_id
-        self.product_name = product_name
-        self.product_options = product_options
-        self.quantity = quantity
